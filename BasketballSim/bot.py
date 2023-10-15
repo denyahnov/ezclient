0ef34266992a5f4c2aaf604a2ce276182ac95474
denyahnov <60083582+denyahnov@users.noreply.github.com>
2023-10-15 17:34:11 +1100

    Add files via upload

---
+++ BasketballSim/bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext.commands import Bot
+
+import os
+import random
+
+bot = Bot("=",intents=discord.Intents.all())
+
+BOT_TOKEN = r"OTc2MDY3NjI3NzExNjA2Nzg0.GPfo6q.IHY8s4jLMuXyMtYZN9wksOknmdPLEooXenDa0I"
+
+@bot.event
+async def on_ready():
+	print("ready")
+
+@bot.command()
+async def d(ctx):
+	image = os.path.join("images", random.choice(os.listdir("images")))
+
+	message = "{} Pick between these `3` by clicking their corresponding buttons.".format(ctx.author.mention)
+
+	await ctx.reply(message,file=discord.File(image))
+
+bot.run(BOT_TOKEN)
